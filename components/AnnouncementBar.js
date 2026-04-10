685dfe2f3002ab2e5a4c6f42fba95ef5cf3169e6
mlbbneo230-blip <mlbbnei230@gmail.com>
2026-04-11 02:59:54 +0530

    style: exactly match announcement bar and logo ui to energymio

--- components/AnnouncementBar.js
+++ components/AnnouncementBar.js
@@ -2,13 +2,17 @@
 
 export default function AnnouncementBar() {
   return (
-    <details className="announcement-bar">
-      <summary className="announcement-bar__summary">
-        <span>573,000 Watt Hours Generated</span>
-        <svg viewBox="0 0 10 6"><path fillRule="evenodd" clipRule="evenodd" d="M9.354.646a.5.5 0 00-.708 0L5 4.293 1.354.646a.5.5 0 00-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 000-.708z" fill="currentColor"/></svg>
+    <details className="announcement-bar" style={{ background: 'var(--green)', border: 'none' }}>
+      <summary className="announcement-bar__summary" style={{ color: 'var(--white)' }}>
+        <span style={{ fontWeight: 500 }}>573,000 WATT HOURS GENERATED</span>
+        <svg aria-hidden="true" focusable="false" role="presentation" viewBox="0 0 10 6" style={{ color: 'var(--white)' }}>
+          <path fillRule="evenodd" clipRule="evenodd" d="M9.354.646a.5.5 0 00-.708 0L5 4.293 1.354.646a.5.5 0 00-.708.708l4 4a.5.5 0 00.708 0l4-4a.5.5 0 000-.708z" fill="currentColor"></path>
+        </svg>
       </summary>
-      <div className="announcement-bar__content" style={{ backgroundImage: 'linear-gradient(135deg, #0a0a0a 0%, #1a2a1a 100%)' }}>
-        <div className="stat-number">573,000</div>
+      <div className="announcement-bar__content" style={{ backgroundImage: 'url(/images/product-gym.jpg)' }}>
+        <div className="stat-number">
+          <span>573,000</span>
+        </div>
         <h3 style={{ fontSize: '1.6rem', color: 'var(--white)', marginTop: '12px' }}>Watt hours of clean energy generated with Energym</h3>
         <p style={{ color: 'var(--mediumgrey)', marginTop: '12px', fontSize: '1.3rem' }}>Learn how we&apos;re aiming to be Carbon Zero</p>
         <a href="#" className="btn btn-primary" style={{ marginTop: '24px' }}>Our Mission</a>
--- components/Header.js
+++ components/Header.js
@@ -45,7 +45,7 @@ export default function Header() {
 
   const navItems = [
     {
-      label: 'RE:GEN For Home',
+      label: 'RE:GEN FOR HOME',
       textTitle: 'Take Back Your Power',
       links: [
         { text: 'RE:GEN + OHM', img: '/images/product-home.jpg', href: '/pages/regen-ride' },
@@ -53,7 +53,7 @@ export default function Header() {
       ]
     },
     {
-      label: 'RE:GEN For Business',
+      label: 'RE:GEN FOR BUSINESS',
       textTitle: 'Power Up Your Facility',
       links: [
         { text: 'Gyms', img: '/images/product-gym.jpg', href: '/pages/regen-for-business' },
@@ -62,7 +62,7 @@ export default function Header() {
       ]
     },
     {
-      label: 'More',
+      label: 'MORE',
       textTitle: 'Turn Human Power:ON',
       links: [
         { text: 'Mission', img: '/images/tech-platform.jpg', href: '/pages/our-mission' },
@@ -83,10 +83,18 @@ export default function Header() {
         </button>
 
         <Link href="/" className="header__logo" aria-label="Energym Home" onClick={() => setOpenDropdown(null)}>
-          <svg viewBox="0 0 600 55" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <text x="0" y="40" fill={openDropdown ? 'var(--black)' : 'white'} fontFamily="var(--ff)" fontSize="42" fontWeight="400" letterSpacing="8">ENERGYM</text>
+          <svg viewBox="0 0 600.25 55.25" style={{ height: '22px', width: 'auto' }} xmlns="http://www.w3.org/2000/svg">
+            {/* The Stylized R and Registration Mark */}
             <path d="M260.11,25.08l9.71,6.92-13.07,4.15,3.36-11.07Z" fill="#00aa13"/>
             <path d="M272.51,4.49l-10.5,7.34,32.15,.06c.55-.01,1.01,.43,1.02,.98,0,.18-.04,.36-.13,.52l-1.31,2.23c-.27,.46-.72,.78-1.25,.89l-31.32,7.4,46.17,31.35-25.49-27.14,14.65-4.62c1.87-.5,3.53-1.57,4.76-3.07l9.38-12.55c.55-.73,.4-1.76-.33-2.31-.28-.21-.62-.33-.96-.33l-36.8-.73h-.02Z" fill="#00aa13"/>
+            <path d="M600.25,4.47c0,.61-.12,1.21-.35,1.77-.23,.53-.55,1.02-.96,1.43-.42,.41-.91,.73-1.44,.96-.56,.24-1.17,.36-1.78,.35-.62,0-1.23-.11-1.8-.34-.53-.21-1.02-.53-1.42-.94-.4-.41-.72-.89-.93-1.41-.23-.57-.34-1.18-.33-1.8,0-.61,.11-1.21,.35-1.76,.23-.53,.55-1.02,.96-1.43,.41-.41,.9-.73,1.44-.95,.56-.23,1.17-.35,1.77-.34,.62,0,1.23,.11,1.8,.33,.53,.21,1.02,.53,1.42,.93,.4,.41,.72,.89,.93,1.41,.23,.57,.34,1.18,.33,1.79h0Zm-1.04,.07c0-.51-.08-1.01-.26-1.49-.16-.43-.41-.82-.72-1.15-.31-.32-.68-.57-1.09-.73-.44-.17-.91-.26-1.38-.25-.49,0-.98,.09-1.44,.28-.42,.18-.79,.44-1.1,.76-.31,.32-.55,.71-.71,1.13-.17,.44-.25,.9-.25,1.37,0,.5,.08,1.01,.26,1.48,.16,.43,.41,.82,.72,1.15,.31,.32,.68,.57,1.09,.74,.44,.18,.9,.26,1.38,.26,.5,0,.99-.09,1.44-.28,.83-.36,1.48-1.04,1.8-1.88,.17-.44,.25-.9,.25-1.37h0Zm-1.36,2.11s0,.06-.02,.09c-.01,.03-.04,.05-.08,.06-.07,.02-.13,.02-.2,.03-.08,0-.2,.01-.34,.01-.11,0-.21,0-.32-.01-.07,0-.14-.02-.2-.04-.04-.01-.08-.04-.11-.08-.03-.04-.05-.09-.06-.15l-.24-.8c-.06-.22-.17-.43-.33-.59-.16-.12-.36-.18-.57-.17h-.31v1.62c0,.07-.03,.14-.1,.18-.15,.04-.3,.05-.46,.05-.16,0-.32,0-.47-.05-.07-.02-.12-.1-.11-.18V2.47c0-.14,.04-.29,.13-.4,.1-.1,.24-.16,.38-.15h1.32c.27,0,.54,.02,.81,.08,.21,.05,.42,.13,.6,.25,.16,.11,.29,.26,.37,.43,.09,.19,.13,.41,.13,.62,.02,.32-.09,.63-.3,.86-.22,.22-.5,.37-.81,.43,.19,.07,.37,.17,.52,.31,.19,.19,.33,.41,.42,.66l.28,.74c.04,.11,.07,.22,.08,.33h0Zm-1.43-3.2c0-.08-.01-.16-.03-.24-.02-.08-.07-.15-.14-.2-.08-.06-.16-.11-.26-.13-.13-.03-.27-.05-.41-.05h-.53v1.29h.51c.24,.03,.48-.04,.67-.19,.13-.13,.19-.3,.19-.48h0Z" fill="#fff" />
+            {/* The Extracted ENERGYM text paths */}
+            <path d="M123.31,35.25h-3.39V16.93c0-1.03-.84-1.86-1.87-1.87h-27.05c-1.03,0-1.87,.83-1.87,1.86h0v18.32h-3.39V16.93c0-2.9,2.36-5.24,5.26-5.24h27.05c2.9,0,5.25,2.35,5.25,5.24v18.32Z" fill="#fff" stroke="#fff" strokeWidth=".5px" />
+            <path d="M5.54,15.06H37.8v-3.38H5.54c-2.91,0-5.27,2.34-5.29,5.24v13.09c.02,2.9,2.38,5.24,5.28,5.24H37.8v-3.38H5.53c-1.03,0-1.87-.83-1.87-1.86v-4.5h30.42v-3.38H3.66v-5.2c0-1.03,.85-1.86,1.88-1.86h0Z" fill="#fff" stroke="#fff" strokeWidth=".5px" />
+            <path d="M176.54,15.06h32.27v-3.38h-32.27c-2.91,0-5.27,2.34-5.29,5.24v13.09c.02,2.9,2.38,5.24,5.28,5.24h32.28v-3.38h-32.27c-1.03,0-1.87-.83-1.87-1.86v-4.5h30.42v-3.38h-30.42v-5.2c0-1.03,.85-1.86,1.88-1.86h0Z" fill="#fff" stroke="#fff" strokeWidth=".5px" />
+            <path d="M364.18,35.25c-1.4,0-2.74-.55-3.74-1.53-.99-.98-1.55-2.32-1.55-3.71v-13.09c.02-2.91,2.38-5.25,5.29-5.24h32.27v3.38h-32.27c-1.03,0-1.87,.83-1.88,1.86h0v13.08c0,.5,.2,.97,.55,1.32,.35,.35,.83,.55,1.33,.55h26.54c1.03,0,1.87-.84,1.88-1.88h0v-5.28h3.42v5.28c-.02,2.91-2.38,5.25-5.29,5.25h-26.54s-.01,0-.01,0Z" fill="#fff" stroke="#fff" strokeWidth=".5px" />
+            <path d="M481.94,11.68l-3.45,1.55v3.9c.02,.52-.28,.99-.74,1.21l-14.1,6.78-14.97-6.78-.09-.03c-.48-.21-.78-.69-.76-1.21v-3.87l-3.44-1.55v5.42c-.02,1.94,1.14,3.7,2.93,4.46l14.64,6.63v7.06h3.45v-7.09l13.69-6.59c1.76-.78,2.88-2.54,2.85-4.47v-5.42h0Z" fill="#fff" stroke="#fff" strokeWidth=".5px" />
+            <path d="M567.25,16.66c-.1-2.81-2.43-5.03-5.25-4.98h-26.89c-2.85-.02-5.19,2.27-5.23,5.12v18.45h3.39V16.8c0-1.02,.83-1.86,1.85-1.88h11.75v18.01h3.39V14.92h11.74c1.04-.02,1.93,.74,2.06,1.78v18.56h3.2V16.66h0Z" fill="#fff" stroke="#fff" strokeWidth=".5px" />
           </svg>
         </Link>
 
@@ -124,10 +132,15 @@ export default function Header() {
         </nav>
 
         <div className="header__icons">
-          <a href="#" className="header__icon" aria-label="Search">
+          <a href="/search" className="header__icon" aria-label="Search">
             <svg viewBox="0 0 18 19" fill="none"><path fillRule="evenodd" clipRule="evenodd" d="M11.03 11.68A5.784 5.784 0 112.85 3.5a5.784 5.784 0 018.18 8.18zm.26 1.12a6.78 6.78 0 11.72-.7l5.4 5.4a.5.5 0 11-.71.7l-5.41-5.4z" fill="currentColor"/></svg>
           </a>
-          <a href="#" className="header__icon header__cart-icon" aria-label="Cart">
+          <a href="/account" className="header__icon" aria-label="Account">
+            <svg viewBox="0 0 18 19" fill="none" stroke="currentColor" strokeWidth="1.5">
+              <path d="M9 9a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm-6.5 9c0-3.5 2.5-6.5 6.5-6.5s6.5 3 6.5 6.5" />
+            </svg>
+          </a>
+          <a href="/cart" className="header__icon header__cart-icon" aria-label="Cart">
             <svg viewBox="0 0 46 40" fill="none">
               <polyline points="10.53 8 11 1 1 1" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
               <polyline points="7.02 28.6 33.57 28.51 44.53 8 10.53 8" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" fill="none"/>
